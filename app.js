68b433b9c76346c6a6d0c174b1a9a294b60a4d82
stevehotcodes <stevehotcodes@gmail.com>
2023-04-25 10:08:45 +0300

    updates

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ class Product{
           <p>${this.product.productDescription}</p>
           <button onclick="new Product().updateProduct(${this.product.id})">update</button>
          <button onclick="new Product().deleteProduct(${this.product.id})" ><ion-icon name="trash-outline"></ion-icon></button>
+         <button onclick="new Cart().addProduct(${this.product.id})">add to cart</button>
         </div>
      </div>
         
@@ -128,7 +129,100 @@ class App{
         app.innerHTML=htmlProducts
     }    
 }
+let cartDB = {}//
+class Cart{
 
+    addProduct(id){
+        
+        if (cartDB.hasOwnProperty(id)) {
+            cartDB[id].quantity +=1
+        } else {
+            cartDB[id]={quantity:1};
+            // console.log(cartDB);
+        }
+
+       
+        this.render();
+    }
+
+     async  render(){
+        let cartCont=document.getElementById("cartcont");
+        cartCont.innerHTML=" ";
+        console.log(cartDB);
+        
+        for (const index in cartDB) {
+            let cart_el=document.createElement("div");
+            
+            // console.log(index)
+            let response=await fetch(`http://localhost:3000/products/${index}`)
+            let productDetails= await response.json();
+            let cart_el_name=document.createElement("div");
+            let cart_el_price=document.createElement("div");
+            let cart_el_img=document.createElement("div");
+            let cart_el_description=document.createElement("div"); 
+
+            cart_el_name.innerHTML=productDetails.productName;
+            cart_el_price.innerHTML=productDetails.productPrice
+            cart_el_img.innerHTML=productDetails.productImg
+            cart_el_description.innerHTML=productDetails.productDescription;
+
+            let minus=document.createElement("button");
+            let plus=document.createElement("button");
+            let del=document.createElement("button");
+
+             minus.innerHTML="minus";
+             minus.addEventListener("click",()=>{
+                if (cartDB[index]["quantity"]==1) {
+                    this.deleteCartProduct(index);
+                    this.render()
+                } else {
+                    console.log(index)
+                    console.log(cartDB[index])
+                    
+                    cartDB[index]["quantity"]-=1
+                    console.log(cartDB[index]["quantity"])
+                    this.render()
+                }
+             })
+             plus.innerHTML="plus";
+             plus.addEventListener('click',()=>{
+                this.addProduct(index);
+                this.render();
+             })
+             del.innerHTML="delete";
+             del.addEventListener("click",()=>{
+                this.deleteCartProduct(index);
+                this.render();
+             })
+
+    
+
+
+            cart_el.append(cart_el_name,cart_el_price,cart_el_img,minus,plus,del);
+
+       cartCont.appendChild(cart_el);
+        }
+        let totalCart
+    }
+    deleteCartProduct(id){
+        delete cartDB[id]
+    }
+    }    
+//        this.prePopulate(product)
+//        const addToCArt = document.querySelector("#addToCArt")
+//        btn.addEventListener('click', (e)=>{
+//         e.preventDefault()
+        
+//         const updatedProduct= new Product().readValues();
+//         if(btn.innerText==="Added to Cart"){
+//             console.log("added to cart");
+//             this.sendUpdate({...addProduct, id})
+//            }
+//        })
+
+//     }
+//     }
+// }
 
 App.Init()
 // class CustomElement extends HTMLElement{
